9bb2c3dae855a51877e9645d58e766059d21c407
unknown <oleksiyo@mak019.compatibl.com>
2013-01-08 16:45:54 +0200

    Add new method

---
+++ GenerateNumbers/GenerateNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateNumbers
+{
+    public class GenerateNumbers
+    {
+        public List<int> Generate(int start, int end)
+        {
+            return Enumerable.Range(start, end).ToList();
+        }
+    }
+}
---
+++ GenerateNumbers/IGenerateNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateNumbers
+{
+   public interface IGenerateNumbers
+    {
+        List<int> Generate(int start, int end);
+    }
+}
